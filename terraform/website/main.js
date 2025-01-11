57b5be3ad702559eb312cbc2d20080ee1c687e18
fritzalbrecht <fritzalbrecht2@gmail.com>
2025-01-10 21:01:26 -0500

    fix:lambda urls

--- terraform/website/main.js
+++ terraform/website/main.js
@@ -1,6 +1,6 @@
 visitorCount = document.getElementById('visitors');
 
-fetch('https://20sjf7lu58.execute-api.us-east-1.amazonaws.com/prod/putVisitors')
-    .then(() => fetch('https://oe1nrmsfc1.execute-api.us-east-1.amazonaws.com/default/getVIsitors'))
+fetch('https://5lojgsv5w4.execute-api.us-east-1.amazonaws.com/prod/post-visitors-tf')
+    .then(() => fetch('https://5lojgsv5w4.execute-api.us-east-1.amazonaws.com/prod/get-visitors-tf'))
     .then(response => response.json())
     .then(data => (visitorCount.innerHTML = (data)));
