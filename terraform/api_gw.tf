0167519084ccbb43477586c5098c101709e1d7c9
fritzalbrecht <fritzalbrecht2@gmail.com>
2025-01-10 22:23:05 -0500

    feat:cors modules

--- terraform/api_gw.tf
+++ terraform/api_gw.tf
@@ -113,138 +113,18 @@ resource "aws_api_gateway_integration_response" "post_visitors_post_integration_
   }
 }
 
-resource "aws_api_gateway_method" "get_visitors_options" {
-  rest_api_id      = aws_api_gateway_rest_api.cloud_resume_website_visitor_count_rest_api.id
-  resource_id      = aws_api_gateway_resource.get_visitors_resource.id
-  http_method      = "OPTIONS"
-  authorization    = "NONE"
-  api_key_required = false
+module "get_cors" {
+  source = "squidfunk/api-gateway-enable-cors/aws"
+  version = "0.3.3"
+
+  api_id          = aws_api_gateway_rest_api.cloud_resume_website_visitor_count_rest_api.id
+  api_resource_id = aws_api_gateway_resource.get_visitors_resource.id
 }
 
-resource "aws_api_gateway_method_response" "get_visitors_options" {
-  rest_api_id = aws_api_gateway_rest_api.cloud_resume_website_visitor_count_rest_api.id
-  resource_id = aws_api_gateway_resource.get_visitors_resource.id
-  http_method = aws_api_gateway_method.get_visitors_options.http_method
-  status_code = "200"
-  response_models = {
-    "application/json" = "Empty"
-  }
-  response_parameters = {
-    "method.response.header.Access-Control-Allow-Headers" = true
-    "method.response.header.Access-Control-Allow-Methods" = true
-    "method.response.header.Access-Control-Allow-Origin"  = true
-  }
+module "postcors" {
+  source = "squidfunk/api-gateway-enable-cors/aws"
+  version = "0.3.3"
+
+  api_id          = aws_api_gateway_rest_api.cloud_resume_website_visitor_count_rest_api.id
+  api_resource_id = aws_api_gateway_resource.post_visitors_resource.id
 }
-
-resource "aws_api_gateway_integration" "get_visitors_options" {
-  rest_api_id          = aws_api_gateway_rest_api.cloud_resume_website_visitor_count_rest_api.id
-  resource_id          = aws_api_gateway_resource.get_visitors_resource.id
-  http_method          = "OPTIONS"
-  type                 = "MOCK"
-  passthrough_behavior = "WHEN_NO_MATCH"
-  request_templates = {
-    "application/json" : "{\"statusCode\": 200}"
-  }
-}
-
-resource "aws_api_gateway_integration_response" "get_visitors_options" {
-  rest_api_id = aws_api_gateway_rest_api.cloud_resume_website_visitor_count_rest_api.id
-  resource_id = aws_api_gateway_resource.get_visitors_resource.id
-  http_method = aws_api_gateway_integration.get_visitors_options.http_method
-  status_code = "200"
-  response_parameters = {
-    "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'"
-    "method.response.header.Access-Control-Allow-Methods" = "'POST,OPTIONS'"
-    "method.response.header.Access-Control-Allow-Origin"  = "'*'"
-  }
-}
-
-resource "aws_api_gateway_method" "post_visitors_options" {
-  rest_api_id      = aws_api_gateway_rest_api.cloud_resume_website_visitor_count_rest_api.id
-  resource_id      = aws_api_gateway_resource.post_visitors_resource.id
-  http_method      = "OPTIONS"
-  authorization    = "NONE"
-  api_key_required = false
-}
-
-resource "aws_api_gateway_method_response" "post_visitors_options" {
-  rest_api_id = aws_api_gateway_rest_api.cloud_resume_website_visitor_count_rest_api.id
-  resource_id = aws_api_gateway_resource.post_visitors_resource.id
-  http_method = aws_api_gateway_method.post_visitors_options.http_method
-  status_code = "200"
-  response_models = {
-    "application/json" = "Empty"
-  }
-  response_parameters = {
-    "method.response.header.Access-Control-Allow-Headers" = true
-    "method.response.header.Access-Control-Allow-Methods" = true
-    "method.response.header.Access-Control-Allow-Origin"  = true
-  }
-}
-
-resource "aws_api_gateway_integration" "post_visitors_options" {
-  rest_api_id          = aws_api_gateway_rest_api.cloud_resume_website_visitor_count_rest_api.id
-  resource_id          = aws_api_gateway_resource.post_visitors_resource.id
-  http_method          = "OPTIONS"
-  type                 = "MOCK"
-  passthrough_behavior = "WHEN_NO_MATCH"
-  request_templates = {
-    "application/json" : "{\"statusCode\": 200}"
-  }
-}
-
-resource "aws_api_gateway_integration_response" "post_visitors_options" {
-  rest_api_id = aws_api_gateway_rest_api.cloud_resume_website_visitor_count_rest_api.id
-  resource_id = aws_api_gateway_resource.post_visitors_resource.id
-  http_method = aws_api_gateway_integration.post_visitors_options.http_method
-  status_code = "200"
-  response_parameters = {
-    "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'"
-    "method.response.header.Access-Control-Allow-Methods" = "'POST,OPTIONS'"
-    "method.response.header.Access-Control-Allow-Origin"  = "'*'"
-  }
-}
-
-resource "aws_api_gateway_deployment" "cloud_resume_website_visitor_count_rest_api_deployment" {
-  rest_api_id = aws_api_gateway_rest_api.cloud_resume_website_visitor_count_rest_api.id
-
-  triggers = {
-    redeployment = sha1(jsonencode(aws_api_gateway_rest_api.cloud_resume_website_visitor_count_rest_api.body))
-  }
-
-  lifecycle {
-    create_before_destroy = true
-  }
-}
-
-resource "aws_api_gateway_stage" "cloud_resume_website_visitor_count_rest_api_stage" {
-  deployment_id = aws_api_gateway_deployment.cloud_resume_website_visitor_count_rest_api_deployment.id
-  rest_api_id   = aws_api_gateway_rest_api.cloud_resume_website_visitor_count_rest_api.id
-  stage_name    = "prod"
-}
-
-resource "aws_api_gateway_method_settings" "all" {
-  rest_api_id = aws_api_gateway_rest_api.cloud_resume_website_visitor_count_rest_api.id
-  stage_name  = aws_api_gateway_stage.cloud_resume_website_visitor_count_rest_api_stage.stage_name
-  method_path = "*/*"
-  settings {
-    metrics_enabled = true
-    logging_level   = "ERROR"
-  }
-}
-
-resource "aws_api_gateway_domain_name" "fritzalbrecht" {
-  domain_name              = "api-tf.fritzalbrecht.com"
-  regional_certificate_arn = var.acm_cert_arn
-
-  endpoint_configuration {
-    types = ["REGIONAL"]
-  }
-}
-
-resource "aws_api_gateway_base_path_mapping" "fritzalbrecht_base_mapping" {
-  api_id      = aws_api_gateway_rest_api.cloud_resume_website_visitor_count_rest_api.id
-  stage_name  = aws_api_gateway_stage.cloud_resume_website_visitor_count_rest_api_stage.stage_name
-  domain_name = aws_api_gateway_domain_name.fritzalbrecht.domain_name
-}
-
