6e744455007be4ecbf91bb831b04ecc2fc23661a
fritzalbrecht <fritzalbrecht2@gmail.com>
2025-01-10 19:31:35 -0500

    feat:cf record

--- terraform/cloudflare_dns.tf
+++ terraform/cloudflare_dns.tf
@@ -47,7 +47,7 @@ resource "cloudflare_record" "api_gateway_terraform_dns" {
   proxied = false
 }
 
-resource "cloudflare_record" "api_gateway_terraform_dns" {
+resource "cloudflare_record" "api_gateway_invoke_url_record" {
   zone_id = var.cloudflare_zone_id
   name    = "api"
   value   = "${aws_api_gateway_deployment.cloud_resume_website_visitor_count_rest_api_deployment.invoke_url}"
