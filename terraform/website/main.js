8f12e0c95a284d39bee77a47c3a36daede966980
fritzalbrecht <fritzalbrecht2@gmail.com>
2025-01-14 22:59:34 -0500

    fix:js

--- terraform/website/main.js
+++ terraform/website/main.js
@@ -1,8 +1,13 @@
-visitorCount = document.getElementById('visitors');
+visitorCount = document.getElementById('visitorCount');
 
 fetch('https://ryse15m27c.execute-api.us-east-1.amazonaws.com/prod/post-visitors', {
     method: 'POST',
 })
     .then(() => fetch('https://ryse15m27c.execute-api.us-east-1.amazonaws.com/prod/get-visitors'))
     .then(response => response.json())
-    .then(data => (visitorCount.innerHTML = (data)));
+    .then(data => {
+        visitorCount.innerHTML = JSON.stringify(data);
+    })
+    .catch(error => {
+        visitorCount.innerHTML = 'Error: ' + error;
+    });
